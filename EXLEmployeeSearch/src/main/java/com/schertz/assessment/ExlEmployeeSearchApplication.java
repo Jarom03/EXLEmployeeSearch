8c445f487b35e7cb127bec00590e7d8131d80b31
Jarom Schertz <jaromschertz@gmail.com>
2020-07-24 17:57:15 -0400

    Initial Commit

---
+++ EXLEmployeeSearch/src/main/java/com/schertz/assessment/ExlEmployeeSearchApplication.java
@@ -0,0 +1,13 @@
+package com.schertz.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExlEmployeeSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExlEmployeeSearchApplication.class, args);
+	}
+
+}
